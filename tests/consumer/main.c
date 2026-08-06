3193b133c3edb75fa5c6bba4a3a496f83657a509
Zoey Rose <zoey@zoeysr.com>
2026-08-06 12:20:32 +0000

    ci: validate and release libatrinik

---
+++ tests/consumer/main.c
@@ -0,0 +1,16 @@
+#include <toolkit/packet.h>
+#include <atrinik/protocol/game_commands.h>
+
+int main(void)
+{
+    toolkit_import(packet);
+    packet_struct *packet = packet_new(SERVER_CMD_KEEPALIVE, 0, 0);
+    if (packet == NULL) {
+        toolkit_deinit();
+        return 1;
+    }
+
+    packet_free(packet);
+    toolkit_deinit();
+    return 0;
+}
